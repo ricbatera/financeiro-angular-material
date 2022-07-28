55d8d6e58f855b5894e59aa9f06e88267ac4ef52
Ricardo Alves Roberto <contato@rdrtech.com.br>
2022-07-28 19:18:55 -0300

    implementado nova entrada

--- src/app/sistema/entradas/entradas.module.ts
+++ src/app/sistema/entradas/entradas.module.ts
@@ -4,6 +4,28 @@ import { EntradasComponent } from './entradas.component';
 import { NovaEntradaComponent } from './nova-entrada/nova-entrada.component';
 import { EntradasRoutingModule } from './entradas-routing.module';
 import { ListaEntradasComponent } from './lista-entradas/lista-entradas.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ComponentesGeraisModule } from 'src/app/componentes-gerais/componentes-gerais.module';
+import { CurrencyMaskModule } from 'ng2-currency-mask';
+
+//componentes material
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input'; 
+import {MatIconModule} from '@angular/material/icon';
+import {MatTableModule} from '@angular/material/table';
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatSelectModule} from '@angular/material/select';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatSortModule} from '@angular/material/sort';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatCardModule} from '@angular/material/card';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatButtonToggleModule} from '@angular/material/button-toggle'
+import {MatCheckboxModule} from '@angular/material/checkbox'
 
 
 
@@ -15,7 +37,27 @@ import { ListaEntradasComponent } from './lista-entradas/lista-entradas.componen
   ],
   imports: [
     CommonModule,
-    EntradasRoutingModule
+    EntradasRoutingModule,
+    ReactiveFormsModule,
+    ComponentesGeraisModule,
+    CurrencyMaskModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatTableModule,
+    MatNativeDateModule,
+    MatSelectModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatTooltipModule,
+    MatCardModule,
+    MatDividerModule,
+    MatSnackBarModule,
+    MatDialogModule,
+    MatButtonToggleModule,
+    MatCheckboxModule
   ]
 })
 export class EntradasModule { }
--- src/app/sistema/entradas/nova-entrada/nova-entrada.component.ts
+++ src/app/sistema/entradas/nova-entrada/nova-entrada.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 import { Observable } from 'rxjs';
 import { EmpresasJavaGas } from 'src/app/model/empresas-javagas';
@@ -12,15 +15,28 @@ import { DatabaseServiceService } from 'src/app/services/database-service.servic
 })
 export class NovaEntradaComponent implements OnInit {
   $: any;
-  listaObras?: Observable<Obra[]>
-  listaEmrpresasJavaGas?: Observable<EmpresasJavaGas[]>
+  obrasList: Observable<Obra[]>
+  empresasJavaList: Observable<EmpresasJavaGas[]>
+  formulario: FormGroup;
 
   constructor(
-    private db : DatabaseServiceService
+    private db : DatabaseServiceService,
+    private formBuilder: FormBuilder,
+    private route: Router,
+    private _snackBar: MatSnackBar
   ) {
+    this.obrasList = this.db.listarTodasObrasAtivas();
+    this.empresasJavaList = this.db.listarEmpresasJavaGas();
 
-    this.listaObras = this.db.listarTodasObrasAtivas();
-    this.listaEmrpresasJavaGas = this.db.listarEmpresasJavaGas();
+    this.formulario = this.formBuilder.group({
+      descricao: [null, [Validators.required]],
+      empresaId: [null, [Validators.required]],
+      valorEsperado: [null, [Validators.required]],
+      dataVencimento: [null, [Validators.required]],
+      obs: [null, [Validators.required]],
+      obraId: [null, [Validators.required]],
+      recebido: [null, [Validators.required]]
+    });
    }
 
   ngOnInit(): void {
@@ -29,6 +45,37 @@ export class NovaEntradaComponent implements OnInit {
   
   carregaEmpresas(){
     console.log("iniciando")
+  }
+
+  salvar(){
+    console.log('Iniciando o processo de salvamento da nova entrada');
+    if (this.formulario?.valid) {
+      // this.db.novaSaida(this.formulario.value).subscribe(res=>{
+      //   this._snackBar.open('Salvo com sucesso', 'Ok', {duration: 2000})
+      //   this.route.navigate([`sistema/saidas/lista-saida`])
+      // })
+      // this.limparForm();
+    } else {
+      Object.keys(this.formulario.controls).forEach(e => {
+        const campo: any = this.formulario?.get(e);
+        campo.markAsTouched();
+        this._snackBar.open('Preencha todos os campos obrigatórios do formulário!', 'Fechar', {duration: 3000})
+      })
+    }
+    // console.log(this.formulario);
+    console.log(this.formulario.value);
+  }
+
+  limparForm() {
+    this.formulario?.reset();
+  }
+
+  validaCampo(campo: string) {
+    const c: any = this.formulario?.get(campo)
+    if (c.touched) {
+      return c.valid;
+    }
+    return true;
   }
 
 }
